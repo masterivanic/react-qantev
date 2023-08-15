0b2fc9059dc5ce8303bdc92cf585258f0def5052
Philippe Ivan Mbarga <pmbarga@simco.fr>
2023-08-15 10:24:00 +0200

    Home, Navbar component and townjs file created

---
+++ src/data/town.js
@@ -0,0 +1,113 @@
+export const towns = [
+    {
+      "address": "1 Farrer Park Station Rd, Singapore 217562",
+      "phone_number": "6352 1381",
+      "name": "Agewell Seniors Clinic, Agewell Artsz Medical Group Pte Ltd",
+      "coordinates": [1.3127619, 103.8545562],
+      "id": 624,
+      "total_cost": 87552298,
+      "average_patient_age": 36,
+      "average_inpatient_claim_cost": 4154,
+      "average_outpatient_claim_cost": 94
+    },
+    {
+      "address": "290 Orchard Rd, #09-07 The Paragon, Singapore 238859",
+      "phone_number": "6734 3636",
+      "name": "Tan Chue Tin Clinic Pte. Ltd.",
+      "coordinates": [1.30451, 103.836184],
+      "id": 2239,
+      "total_cost": 46698518,
+      "average_patient_age": 46,
+      "average_inpatient_claim_cost": 2189,
+      "average_outpatient_claim_cost": 475
+    },
+    {
+      "address": "Gleneagles Medical Centre #05-02, 6 Napier Road (S)258499, Singapore 258500",
+      "phone_number": "6734 6333",
+      "name": "Anthony Heng Surgery",
+      "coordinates": [1.3077263, 103.8200865],
+      "id": 837,
+      "total_cost": 4137598,
+      "average_patient_age": 44,
+      "average_inpatient_claim_cost": 3464,
+      "average_outpatient_claim_cost": 195
+    },
+    {
+      "address": "Novena Medical Center, 10 Sinaran Dr, #11-07/08, Singapore",
+      "phone_number": "8129 9120",
+      "name": "M-Aesthetic Clinic",
+      "coordinates": [1.3207959, 103.8441874],
+      "id": 890,
+      "total_cost": 90342626,
+      "average_patient_age": 36,
+      "average_inpatient_claim_cost": 1397,
+      "average_outpatient_claim_cost": 261
+    },
+    {
+      "address": "4190 Ang Mo Kio Ave 6, #03-03, Singapore 569841",
+      "phone_number": "6554 6868",
+      "name": "1-Health Day Surgery & Specialist Clinics",
+      "coordinates": [1.3718617, 103.8458448],
+      "id": 425,
+      "total_cost": 62708256,
+      "average_patient_age": 38,
+      "average_inpatient_claim_cost": 1791,
+      "average_outpatient_claim_cost": 491
+    },
+    {
+      "address": "318 Jurong East Street 31, Singapore 600318",
+      "phone_number": "6563 1206",
+      "name": "Eastern Clinic & Surgery",
+      "coordinates": [1.3476749, 103.730477],
+      "id": 2227,
+      "total_cost": 48149896,
+      "average_patient_age": 39,
+      "average_inpatient_claim_cost": 1358,
+      "average_outpatient_claim_cost": 286
+    },
+    {
+      "address": "101 Irrawaddy Rd, #14-12/13/14, Singapore 329565",
+      "phone_number": "6735 8180",
+      "name": "Freia Medical",
+      "coordinates": [1.3206053, 103.8432372],
+      "id": 938,
+      "total_cost": 20338505,
+      "average_patient_age": 57,
+      "average_inpatient_claim_cost": 4133,
+      "average_outpatient_claim_cost": 494
+    },
+    {
+      "address": "9 Scotts Rd, #06-01/02/03 Pacific Plaza, Singpaore, Singapore 228210",
+      "phone_number": "6733 2002",
+      "name": "AEON Medical and Aesthetic Centre",
+      "coordinates": [1.3065552, 103.8321944],
+      "id": 1063,
+      "total_cost": 41391081,
+      "average_patient_age": 59,
+      "average_inpatient_claim_cost": 2239,
+      "average_outpatient_claim_cost": 71
+    },
+    {
+      "address": "181 Kitchener Rd, #01-01 Parkroyal on, Singapore 208533",
+      "phone_number": "6910 5388",
+      "name": "Urgent Care Clinic International - UCC",
+      "coordinates": [1.3106349, 103.8557348],
+      "id": 1222,
+      "total_cost": 89594546,
+      "average_patient_age": 47,
+      "average_inpatient_claim_cost": 3384,
+      "average_outpatient_claim_cost": 249
+    },
+    {
+      "address": "6A Napier Road, Gleneagles Hospital, #03-38 Annexe Block, Singapore 258500",
+      "phone_number": "6472 2022",
+      "name": "The Brain & Spine Clinic - Dr Timothy Lee",
+      "coordinates": [1.307063, 103.819973],
+      "id": 1798,
+      "total_cost": 67365746,
+      "average_patient_age": 50,
+      "average_inpatient_claim_cost": 3394,
+      "average_outpatient_claim_cost": 168
+    }
+  ]
+  
